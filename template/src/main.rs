639ef24c6aef967ce313dc5192671c595c48f2cf
Roy Arvenas <roy.arvenas@helicon.ai>
2024-10-17 16:34:34 +0200

    Caching input in input.txt

--- template/src/main.rs
+++ template/src/main.rs
@@ -2,24 +2,38 @@ mod part1;
 mod part2;
 mod utils;
 use aoc_client::{AocClient, AocResult};
+use std::{fs, path::Path};
 
-fn get_aoc_input() -> AocResult<String> {
+const INPUT_FILE: &str = "./input.txt";
+
+fn get_input_from_aoc() -> AocResult<String> {
     let client = AocClient::builder()
         .session_cookie_from_default_locations()?
-        .year(2024)?
+        .year(2023)?
         .day(1)?
         .build()?;
 
+    println!("Getting input from AoC site...");
     let input: String = client.get_input()?;
     Ok(input)
 }
 
+fn get_input() -> String {
+    match Path::new(INPUT_FILE).exists() {
+        true => fs::read_to_string(INPUT_FILE).unwrap(),
+        false => match get_input_from_aoc() {
+            Ok(input) => {
+                fs::write(INPUT_FILE, &input).expect("Failed to write input file!");
+                input
+            }
+            Err(e) => panic!("AoC client error: {}", e),
+        },
+    }
+}
+
 fn main() {
-    // this should probably be written to disk, so I don't grab it everytime...
-    let input = match get_aoc_input() {
-        Ok(input) => input,
-        Err(e) => panic!("AoC client error: {}", e),
-    };
+    let input = get_input();
+    println!("Input: {}", input);
     println!("Processing part1...");
     let part1_result = part1::run(&input);
     println!("Processing part2...");
