cd6fa9cc19350150bb8783cb72df061ea444130c
Roy Arvenas <roy.arvenas@helicon.ai>
2024-10-18 11:25:19 +0200

    Day automatically picked up from directory name

--- template/src/main.rs
+++ template/src/main.rs
@@ -2,18 +2,16 @@ mod part1;
 mod part2;
 mod utils;
 use aoc_client::{AocClient, AocResult};
-use std::{fs, path::Path};
+use std::{env, fs, path::Path};
 
-const YEAR: i32 = 2024;
-// TODO: Pick up day automatically from directory instead...
-const DAY: u32 = 1;
+const YEAR: i32 = 2023;
 const INPUT_FILE: &str = "./input.txt";
 
-fn get_input_from_aoc() -> AocResult<String> {
+fn get_input_from_aoc(day: &usize) -> AocResult<String> {
     let client = AocClient::builder()
         .session_cookie_from_default_locations()?
         .year(YEAR)?
-        .day(DAY)?
+        .day(*day as u32)?
         .build()?;
 
     println!("Getting input from AoC site...");
@@ -21,21 +19,41 @@ fn get_input_from_aoc() -> AocResult<String> {
     Ok(input)
 }
 
-fn get_input() -> String {
+fn get_input(day: &usize) -> String {
     match Path::new(INPUT_FILE).exists() {
         true => fs::read_to_string(INPUT_FILE).unwrap(),
-        false => match get_input_from_aoc() {
-            Ok(input) => {
-                fs::write(INPUT_FILE, &input).expect("Failed to write input file!");
-                input
+        false => {
+            println!(
+                "No local input.txt found for day {}, attempting to download from AoC...",
+                day
+            );
+            match get_input_from_aoc(day) {
+                Ok(input) => {
+                    fs::write(INPUT_FILE, &input).expect("Failed to write input file!");
+                    input
+                }
+                Err(e) => panic!("AoC client error: {}", e),
             }
-            Err(e) => panic!("AoC client error: {}", e),
-        },
+        }
     }
 }
+
+fn get_day() -> usize {
+    let cur_path = env::current_dir();
+    if let Ok(path) = cur_path {
+        if let Some(dir_name) = path.as_path().iter().last() {
+            if let Some(day) = dir_name.to_str().unwrap().split("day").last() {
+                return day.parse::<usize>().unwrap();
+            }
+        }
+    }
+    panic!(
+        "Failed to figure out day number, please verify that you are running in a correct directory, ie 'day2'"
+    );
+}
+
 fn main() {
-    let input = get_input();
-    println!("Input: {}", input);
+    let input = get_input(&get_day());
     println!("Processing part1...");
     let part1_result = part1::run(&input);
     println!("Processing part2...");
