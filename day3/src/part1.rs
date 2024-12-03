dcc43eea1cbb08018436e954b607f50a7e7dc536
Roy Arvenas <roy.arvenas@helicon.ai>
2024-12-03 11:10:03 +0100

    Day3.1 Done

---
+++ day3/src/part1.rs
@@ -0,0 +1,56 @@
+#[allow(unused_variables)]
+fn parse(lines: Vec<&str>) -> Vec<(usize, usize)> {
+    let mut multiplications: Vec<(usize, usize)> = vec![];
+    for line in lines {
+        let mut iter = line.split("mul").peekable();
+        if iter.peek() != Some(&"mul") {
+            iter.next();
+        }
+        let potential_mul: Vec<(usize, usize)> = iter
+            .filter(|x| x.starts_with('('))
+            .map(|x| {
+                let mut c = x.chars();
+                c.next();
+                c.as_str()
+                    .split(")")
+                    .next()
+                    .expect("No first value")
+                    .split(",")
+                    .collect()
+            })
+            .filter(|x: &Vec<&str>| x.len() == 2)
+            .map(|x| {
+                let mut iter = x.iter();
+                (
+                    iter.next().unwrap_or(&"0").parse::<usize>().unwrap_or(0),
+                    iter.next().unwrap_or(&"0").parse::<usize>().unwrap_or(0),
+                )
+            })
+            .collect();
+        multiplications.extend(potential_mul)
+    }
+    multiplications
+}
+
+#[allow(unused_variables)]
+pub fn run(input: &str) -> usize {
+    let lines = input.lines().collect::<Vec<_>>();
+    let mul = parse(lines);
+    let mut sum = 0;
+    for pair in mul {
+        sum += pair.0 * pair.1;
+    }
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::run;
+    use std::fs;
+
+    #[test]
+    fn test_part1() {
+        let example_content = fs::read_to_string("example_input1.txt").unwrap();
+        assert_eq!(run(&example_content), 161);
+    }
+}
