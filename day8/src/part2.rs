85562d3803046f5f4b79a35688a74179174b1195
Roy Arvenas <roy.arvenas@gmail.com>
2024-12-08 14:57:42 +0100

    Day 8.2 Done

--- day8/src/part2.rs
+++ day8/src/part2.rs
@@ -1,13 +1,68 @@
+use std::collections::{HashMap, HashSet};
+
+use crate::utils::Vector2D;
+
+type Int = isize;
+type Coord = Vector2D<Int>;
+type SignalType = HashMap<char, Vec<Coord>>;
+type Size = (usize, usize);
+
 #[allow(unused_variables)]
-fn parse(lines: Vec<&str>) {
-    // Code here
+fn parse(lines: Vec<&str>) -> (SignalType, Size) {
+    let mut signals: SignalType = HashMap::new();
+    let height = lines.len();
+    let width = lines.first().expect("No first row in data").len();
+    for (y, line) in lines.iter().enumerate() {
+        for (x, char) in line.chars().enumerate() {
+            if char != '.' {
+                let pos = Coord::new(x as Int, y as Int);
+                signals.entry(char).or_default().push(pos);
+            }
+        }
+    }
+    (signals, (width, height))
 }
 
+fn is_on_map(pos: &Coord, (width, height): &Size) -> bool {
+    pos.x >= 0 && pos.x < *width as isize && pos.y >= 0 && pos.y < *height as isize
+}
+
+fn calculate_antinodes(sig1: &Coord, sig2: &Coord, size: &Size) -> HashSet<Coord> {
+    let mut antinodes: HashSet<Coord> = HashSet::new();
+    antinodes.insert(*sig1);
+    antinodes.insert(*sig2);
+
+    let node_vec = *sig2 - *sig1;
+    let mut node1 = *sig1 - node_vec;
+    while is_on_map(&node1, size) {
+        antinodes.insert(node1);
+        node1 = node1 - node_vec;
+    }
+
+    let mut node2 = *sig2 + node_vec;
+    while is_on_map(&node2, size) {
+        antinodes.insert(node2);
+        node2 = node2 + node_vec;
+    }
+    antinodes
+}
+
+fn find_nr_antinodes(signals: SignalType, size: Size) -> usize {
+    let mut unique_antinodes: HashSet<Coord> = HashSet::new();
+    for (_, locs) in signals {
+        for (i, loc1) in locs.iter().enumerate() {
+            for loc2 in locs.iter().skip(i + 1) {
+                unique_antinodes.extend(calculate_antinodes(loc1, loc2, &size));
+            }
+        }
+    }
+    unique_antinodes.len()
+}
 #[allow(unused_variables)]
 pub fn run(input: &str) -> usize {
     let lines = input.lines().collect::<Vec<_>>();
-    parse(lines);
-    0
+    let (signals, size) = parse(lines);
+    find_nr_antinodes(signals, size)
 }
 
 #[cfg(test)]
@@ -17,7 +72,13 @@ mod tests {
 
     #[test]
     fn test_part2() {
+        let example_content = fs::read_to_string("example_input1.txt").unwrap();
+        assert_eq!(run(&example_content), 34);
+    }
+
+    #[test]
+    fn test_part2_2() {
         let example_content = fs::read_to_string("example_input2.txt").unwrap();
-        assert_eq!(run(&example_content), 10);
+        assert_eq!(run(&example_content), 9);
     }
 }
