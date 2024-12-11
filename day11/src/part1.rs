7936c020b9dd4f5ebbf369058e23954cbabd4b70
Roy Arvenas <roy.arvenas@gmail.com>
2024-12-11 23:17:50 +0100

    Day 11.2 Done!

--- day11/src/part1.rs
+++ day11/src/part1.rs
@@ -5,35 +5,35 @@ fn parse(line: &str) -> Vec<usize> {
         .collect()
 }
 
-fn blink(mut stones: Vec<usize>) -> Vec<usize> {
-    let mut extra_stones = 0;
-    for i in 0..stones.len() {
-        let stone = stones.remove(i + extra_stones);
-        match stone {
-            0 => {
-                stones.insert(i, 1);
+fn blink_stone(stone: usize, nr_of_blinks: usize) -> usize {
+    let mut stones = vec![stone];
+    for _ in 0..nr_of_blinks {
+        let mut extra_stones = 0;
+        for i in 0..stones.len() {
+            let stone = stones.remove(i + extra_stones);
+            match stone {
+                0 => {
+                    stones.insert(i, 1);
+                }
+                value if (value.ilog10() + 1) % 2 == 0 => {
+                    let split_digits = (value.ilog10() + 1) / 2;
+                    let upper = value / 10usize.pow(split_digits);
+                    let lower = value - upper * 10usize.pow(split_digits);
+                    stones.insert(i, upper);
+                    stones.insert(i + 1, lower);
+                    extra_stones += 1;
+                }
+                value => stones.insert(i, value * 2024),
             }
-            value if (value.ilog10() + 1) % 2 == 0 => {
-                let split_digits = (value.ilog10() + 1) / 2;
-                let upper = value / 10usize.pow(split_digits);
-                let lower = value - upper * 10usize.pow(split_digits);
-                stones.insert(i, upper);
-                stones.insert(i + 1, lower);
-                extra_stones += 1;
-            }
-            value => stones.insert(i, value * 2024),
         }
     }
-    stones
+    stones.len()
 }
 
 #[allow(unused_variables)]
 pub fn run(input: &str) -> usize {
-    let mut stones = parse(input.trim());
-    for i in 0..25 {
-        stones = blink(stones);
-    }
-    stones.len()
+    let stones = parse(input.trim());
+    stones.iter().map(|x| blink_stone(*x, 25)).sum()
 }
 
 #[cfg(test)]
--- day11/src/part2.rs
+++ day11/src/part2.rs
@@ -1,13 +1,60 @@
+use std::collections::HashMap;
+
+const NR_OF_BLINKS: usize = 75;
+type Cache = HashMap<(usize, usize), usize>;
+
 #[allow(unused_variables)]
-fn parse(lines: Vec<&str>) {
-    // Code here
+fn parse(line: &str) -> Vec<usize> {
+    line.split_whitespace()
+        .map(|x| x.parse::<usize>().expect("Value not a number"))
+        .collect()
+}
+
+fn blink_stone(stone: usize, nr_of_blinks: usize, mut cache: Cache) -> (usize, Cache) {
+    let mut sum = 0;
+    if nr_of_blinks == 0 {
+        return (1, cache);
+    }
+    if let Some(result) = cache.get(&(stone, nr_of_blinks)) {
+        return (*result, cache);
+    }
+    match stone {
+        0 => {
+            let (result, new_cache) = blink_stone(1, nr_of_blinks - 1, cache);
+            sum += result;
+            cache = new_cache;
+        }
+        value if (value.ilog10() + 1) % 2 == 0 => {
+            let split_digits = (value.ilog10() + 1) / 2;
+            let upper = value / 10usize.pow(split_digits);
+            let lower = value - upper * 10usize.pow(split_digits);
+            let (result1, new_cache) = blink_stone(upper, nr_of_blinks - 1, cache);
+            cache = new_cache;
+            let (result2, new_cache) = blink_stone(lower, nr_of_blinks - 1, cache);
+            sum += result1 + result2;
+            cache = new_cache;
+        }
+        value => {
+            let (result, new_cache) = blink_stone(value * 2024, nr_of_blinks - 1, cache);
+            sum += result;
+            cache = new_cache;
+        }
+    }
+    cache.insert((stone, nr_of_blinks), sum);
+    (sum, cache)
 }
 
 #[allow(unused_variables)]
 pub fn run(input: &str) -> usize {
-    let lines = input.lines().collect::<Vec<_>>();
-    parse(lines);
-    0
+    let stones = parse(input.trim());
+    let mut cache: Cache = HashMap::new();
+    let mut sum = 0;
+    for stone in stones {
+        let (stone_len, new_cache) = blink_stone(stone, NR_OF_BLINKS, cache);
+        cache = new_cache;
+        sum += stone_len;
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -18,6 +65,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let example_content = fs::read_to_string("example_input2.txt").unwrap();
-        assert_eq!(run(&example_content), 10);
+        assert_eq!(run(&example_content), 55312);
     }
 }
